f88fa11ca212efce93add7203adca539dfe3f042
haruka1727 <giovydimauro@live.it>
2025-06-09 15:07:17 +0200

    Implementing footer with links

---
+++ src/components/Footer.tsx
@@ -0,0 +1,22 @@
+export default function Footer() {
+  return (
+    <footer className="text-center p-4 text-sm ">
+      Made with ❤️ by{" "}
+      <a
+        href="https://github.com/haruka1727"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="font-semibold text-blue-600 hover:underline"
+      >
+        Gio
+      </a> and <a
+        href="https://github.com/DenysPashkov"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="font-semibold text-blue-600 hover:underline"
+      >
+        Denys
+      </a>
+    </footer>
+  );
+}
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,18 +1,38 @@
-import logo from '../assets/logo.png';
+import logo from "../assets/logo.png";
+import Footer from "./Footer";
 
 function SideBar() {
   return (
     <>
-      <aside className="flex h-screen w-64 bg-blue-300 text-black flex flex-col">
+      <aside className="flex h-screen w-64 bg-blue-300 text-black flex flex-col justify-between">
         <div>
           <img src={logo} alt="Logo" />
+
+          <nav className="flex flex-col p-4 space-y-2">
+            <a
+              href="/dashboard"
+              className="px-3 py-2 rounded hover:bg-gray-100"
+            >
+              Dashboard
+            </a>
+            <a
+              href="/transactions"
+              className="px-3 py-2 rounded hover:bg-gray-100"
+            >
+              Transactions
+            </a>
+            <a href="/budgets" className="px-3 py-2 rounded hover:bg-gray-100">
+              Budgets
+            </a>
+            <a
+              href="/planned-expenses"
+              className="px-3 py-2 rounded hover:bg-gray-100"
+            >
+              Planned expenses
+            </a>
+          </nav>
         </div>
-        <nav className="flex flex-col p-4 space-y-2">
-          <a href="/dashboard" className="px-3 py-2 rounded hover:bg-gray-100">Dashboard</a>
-          <a href="/transactions" className="px-3 py-2 rounded hover:bg-gray-100">Transactions</a>
-          <a href="/budgets" className="px-3 py-2 rounded hover:bg-gray-100">Budgets</a>
-          <a href="/planned-expenses" className="px-3 py-2 rounded hover:bg-gray-100">Planned expenses</a>
-        </nav>
+        <Footer />
       </aside>
     </>
   );
